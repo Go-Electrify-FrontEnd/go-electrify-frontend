f9ff7e8fb7f40999717fdb27f447b72e0a6bb605
quanphung1120 <phungthequan030@gmail.com>
2025-10-06 14:47:37 +0700

    Implements landing page and authentication
    Adds a new landing page for the application, including hero sections, features, and calls to action with Vietnamese translation.
    
    Implements user login page and basic authentication form.
    
    Integrates Aceternity UI registry.
    
    Configures Next.js to allow image optimization from external sources.
    
    Updates dependencies, including adding motion for animations.
    
    Replaces a static image with a webp version.

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,16 @@
-import PurposeWarning from "@/components/shared/purpose-warning";
+import { StickyBanner } from "@/components/ui/sticky-banner";
 
 export default function LandingLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StickyBanner className="z-50 bg-gradient-to-b from-blue-500 to-blue-600">
+        This is demo project, please don't use it
+      </StickyBanner>
+      {children}
+    </main>
+  );
 }
