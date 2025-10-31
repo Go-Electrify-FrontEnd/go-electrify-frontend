0bafce7680da4ad6589ded403599c3907323eaa9
quanphung1120 <phungthequan030@gmail.com>
2025-10-31 07:43:56 +0700

    feat: adding log message to getTransactions() function

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -72,8 +72,9 @@ export function LoginForm({
 
   return (
     <form
-      onSubmit={handleSubmit}
-      className={cn("flex flex-col gap-6", className)}
+    onSubmit={handleSubmit}
+    className={cn("flex flex-col gap-6", className)}
+    data-testid="login-form"
       {...props}
     >
       <div className="flex flex-col items-center gap-2 text-center">
@@ -93,11 +94,12 @@ export function LoginForm({
                   Email
                 </FieldLabel>
                 <Input
-                  {...field}
-                  className="h-[45px]"
-                  placeholder="email@example.com"
-                  aria-invalid={fieldState.invalid}
-                  disabled={pending}
+                {...field}
+                className="h-[45px]"
+                placeholder="email@example.com"
+                aria-invalid={fieldState.invalid}
+                disabled={pending}
+                  data-testid="email-input"
                 />
                 {fieldState.invalid && (
                   <FieldError errors={[fieldState.error]} />
@@ -106,10 +108,10 @@ export function LoginForm({
             )}
           />
         </FieldGroup>
-        <Button type="submit" className="w-full" size="lg" disabled={pending}>
-          <div className="text-base font-medium">
-            {pending ? "Đang gửi..." : "Đăng nhập"}
-          </div>
+        <Button type="submit" className="w-full" size="lg" disabled={pending} data-testid="login-submit-button">
+        <div className="text-base font-medium">
+        {pending ? "Đang gửi..." : "Đăng nhập"}
+        </div>
         </Button>
         <div className="after:border-border relative text-center after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
